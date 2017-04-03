a1dff30f3a95794b752fe5a61c27f428a8472a2c
сергей слиж <sergeislizh@mail.ru>
2017-04-04 01:07:02 +0300

    Add new format + Tests

--- Task1Tests/CustomerTests.cs
+++ Task1Tests/CustomerTests.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +9,67 @@ using Task1;
 
 namespace Task1Tests
 {
+    [TestFixture]
     public class CustomerTests
     {
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100")]
+        public void Customer_ArgumentException(string name, decimal revenue, string contactPhone)
+        {
+            Assert.Throws<ArgumentException>(() => new Customer(name, contactPhone, revenue).ToString("AA"));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100")]
+        public void NewFormat_ArgumentException(string name, decimal revenue, string contactPhone)
+        {
+            Assert.Throws<ArgumentException>(() => new NewFormat().Format("AA", new Customer(name, contactPhone, revenue), null));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", ExpectedResult = "Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        public string Customer_G_PositiveTests(string name, decimal revenue, string contactPhone)
+        {
+            return new Customer(name, contactPhone, revenue).ToString("g", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", ExpectedResult = "Jeffrey Richter")]
+        public string Customer_N_PositiveTests(string name, decimal revenue, string contactPhone)
+        {
+            return new Customer(name, contactPhone, revenue).ToString("n", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", ExpectedResult = "1,000,000.00")]
+        public string Customer_R_PositiveTests(string name, decimal revenue, string contactPhone)
+        {
+            return new Customer(name, contactPhone, revenue).ToString("r", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", ExpectedResult = "+1 (425) 555-0100")]
+        public string Customer_C_PositiveTests(string name, decimal revenue, string contactPhone)
+        {
+            return new Customer(name, contactPhone, revenue).ToString("c", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", ExpectedResult = "Jeffrey Richter, 1,000,000.00")]
+        public string Customer_NR_PositiveTests(string name, decimal revenue, string contactPhone)
+        {
+            return new Customer(name, contactPhone, revenue).ToString("nr", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", ExpectedResult = "Jeffrey Richter, +1 (425) 555-0100")]
+        public string Customer_NC_PositiveTests(string name, decimal revenue, string contactPhone)
+        {
+            return new Customer(name, contactPhone, revenue).ToString("nc", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", ExpectedResult = "1,000,000.00, +1 (425) 555-0100")]
+        public string Customer_RC_PositiveTests(string name, decimal revenue, string contactPhone)
+        {
+            return new Customer(name, contactPhone, revenue).ToString("rc", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", ExpectedResult = "Jeffrey Richter - $1,000,000.00 - +1 (425) 555-0100")]
+        public string NewCustomer_PositiveTests(string name, decimal revenue, string contactPhone)
+        {
+            return new NewFormat().Format("z", new Customer(name, contactPhone, revenue), CultureInfo.CreateSpecificCulture("en-US"));
+        }
     }
 }
--- Task1Tests/NewFormat.cs
+++ Task1Tests/NewFormat.cs
@@ -8,8 +8,19 @@ using Task1;
 
 namespace Task1Tests
 {
+    /// <summary>
+    /// New format for string representation.
+    /// </summary>
     public class NewFormat : IFormatProvider, ICustomFormatter
     {
+        #region ICustomFormatter's method
+        /// <summary>
+        /// Method implements a string representation of Custumer's object.
+        /// </summary>
+        /// <param name="format">Format defines a string representation.</param>
+        /// <param name="arg">Type of object which is used for new string representation.</param>
+        /// <param name="formatProvider">FormatProvider defines culture.</param>
+        /// <returns>A string representation of Custumer's object.</returns>
         public string Format(string format, object arg, IFormatProvider formatProvider)
         {
             if (String.IsNullOrEmpty(format))
@@ -24,15 +35,23 @@ namespace Task1Tests
             Customer customer = (Customer)arg;
 
             if (format.ToUpperInvariant() != "Z")
-                return customer.ToString("G", CultureInfo.CreateSpecificCulture("en-US"));
+                return customer.ToString(format, CultureInfo.CreateSpecificCulture("en-US"));
 
             return string.Format($"{customer.Name} - {customer.Revenue.ToString("C", formatProvider)} - {customer.ContactPhone}");
         }
+        #endregion
 
+        #region IFormatProvider's method
+        /// <summary>
+        /// Method defines if you can create new format.
+        /// </summary>
+        /// <param name="formatType">Type of format.</param>
+        /// <returns>Data about format.</returns>
         public object GetFormat(Type formatType)
         {
             if (formatType == typeof(ICustomFormatter)) return this;
             return null;
         }
+        #endregion
     }
 }
